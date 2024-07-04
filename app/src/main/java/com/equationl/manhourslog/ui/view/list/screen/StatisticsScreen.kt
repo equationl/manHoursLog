4ed8146d1acc3adbdfaa51f2743f156fba0e4b21
equationl <equationl@qq.com>
2024-07-04 16:12:40 +0800

    优化统计页标题

--- app/src/main/java/com/equationl/manhourslog/ui/view/list/screen/StatisticsScreen.kt
+++ app/src/main/java/com/equationl/manhourslog/ui/view/list/screen/StatisticsScreen.kt
@@ -36,6 +36,7 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButton
@@ -46,9 +47,11 @@ import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.TooltipDefaults
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.material3.rememberTooltipState
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -58,6 +61,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -88,6 +92,7 @@ import me.bytebeats.views.charts.bar.render.xaxis.SimpleXAxisDrawer
 import me.bytebeats.views.charts.bar.render.yaxis.SimpleYAxisDrawer
 import me.bytebeats.views.charts.simpleChartAnimation
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StatisticsScreen(
     viewModel: StatisticsViewModel = hiltViewModel()
@@ -114,13 +119,26 @@ fun StatisticsScreen(
         }
     }
 
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
+    val collapsedFraction by remember {
+        derivedStateOf {
+            scrollBehavior.state.collapsedFraction
+        }
+    }
+
+
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             TopBar(
                 showType = state.showType,
                 iniDateRangeValue = state.showRange,
                 onFilterDateRange = viewModel::onFilterShowRange,
+                scrollBehavior = scrollBehavior,
+                collapsedFraction = collapsedFraction,
                 onChangeShowType = {
                     viewModel.onChangeShowType(context)
                 },
@@ -177,6 +195,8 @@ fun StatisticsScreen(
 private fun TopBar(
     showType: StatisticsShowType,
     iniDateRangeValue: StatisticsShowRange,
+    scrollBehavior: TopAppBarScrollBehavior,
+    collapsedFraction: Float,
     onFilterDateRange: (value: StatisticsShowRange) -> Unit,
     onChangeShowType: () -> Unit,
     onExport: () -> Unit,
@@ -186,10 +206,16 @@ private fun TopBar(
     val dialogState = rememberMaterialDialogState()
     var isExpandMenu by remember { mutableStateOf(false) }
 
-    TopAppBar(
+    MediumTopAppBar(
         title = {
-            Text(text = "Statistics")
+            if (collapsedFraction >= 0.5f) {
+                Text(text = "Statistics")
+            }
+            else {
+                Text(text = "Statistics(${iniDateRangeValue.start.formatDateTime("yyyyMMdd")}-${iniDateRangeValue.end.formatDateTime("yyyyMMdd")})")
+            }
         },
+        scrollBehavior = scrollBehavior,
         navigationIcon = {
             IconButton(
                 onClick = {
@@ -354,7 +380,6 @@ private fun ListContent(
                                     onClickDeleteItem(item.id)
                                 },
                                 onClickCard = {
-                                    // TODO show note
                                     currentNoteValue = item.note ?: ""
                                     clickItemId = item.id
                                     dialogState.show()
